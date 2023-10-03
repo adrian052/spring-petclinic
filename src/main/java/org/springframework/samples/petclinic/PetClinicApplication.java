110af9e274281c79d1144c3faa7ac702411ae63e
Aleksandr_Smirnov4 <aa.smirnov2@gmail.com>
2023-10-03 14:17:09 -0600

    add security starter and default security bootstrap

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -19,6 +19,7 @@ package org.springframework.samples.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * PetClinic Spring Boot Application.
@@ -28,6 +29,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
  */
 @SpringBootApplication
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
+@EnableWebSecurity
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
